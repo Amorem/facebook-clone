b09b2dd3f16ccf4d9b4fb031e4796e1f31a9200e
Amorem <dev@amorem.com>
2021-05-17 17:52:15 +0200

    add next-auth to handle Facebook login

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
+import { getSession } from "next-auth/client";
 import Head from "next/head";
 import Header from "../components/Header";
+import Login from "../components/Login";
 
-export default function Home() {
+export default function Home({ session }) {
+  console.log(session);
+  if (!session) return <Login />;
   return (
     <div className="">
       <Head>
@@ -16,4 +20,14 @@ export default function Home() {
       </main>
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  // Get the user
+  const session = await getSession(context);
+  return {
+    props: {
+      session,
+    },
+  };
 }

84ec005f0118a944b25495ef1b2c1e48f893af5a
Amorem <dev@amorem.com>
2021-05-18 15:45:34 +0200

    SSR

--- components/Feed.js
+++ components/Feed.js
@@ -2,13 +2,13 @@ import Stories from "./Stories";
 import InputBox from "./InputBox";
 import Posts from "./Posts";
 
-function Feed() {
+function Feed({ posts }) {
   return (
     <div className="flex-grow h-screen pt-6 mr-4 overflow-y-auto pb-44 xl:mr-40 scrollbar-hide">
       <div className="max-w-md mx-auto md:max-w-lg lg:max-w-2xl ">
         <Stories />
         <InputBox />
-        <Posts />
+        <Posts posts={posts} />
       </div>
     </div>
   );

64ee4f34acbad12956c6c6abbb032413d77ab186
Amorem <dev@amorem.com>
2021-05-18 15:00:06 +0200

    Posts

--- components/Feed.js
+++ components/Feed.js
@@ -1,5 +1,6 @@
 import Stories from "./Stories";
 import InputBox from "./InputBox";
+import Posts from "./Posts";
 
 function Feed() {
   return (
@@ -7,6 +8,7 @@ function Feed() {
       <div className="max-w-md mx-auto md:max-w-lg lg:max-w-2xl ">
         <Stories />
         <InputBox />
+        <Posts />
       </div>
     </div>
   );
